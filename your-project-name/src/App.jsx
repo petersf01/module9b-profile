ff93aad17f3439b20e4b46cf91b2d558b52679b4
Peter Fleming <fleming_peter01@student.smc.edu>
2025-07-26 18:01:40 -0700

    Update bio text.

--- your-project-name/src/App.jsx
+++ your-project-name/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div>
       <UserProfile
         name="Jenny Smith"
-        bio="Bla bla bla."
+        bio="A passionate JavaScript developer with a love for building interactive web experiences. Whether it's crafting dynamic user interfaces, experimenting with new frameworks, or solving tricky bugs, they enjoy turning ideas into functional code."
         imageUrl="https://plus.unsplash.com/premium_photo-1690487729258-7dcefc84a532?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
     </div>
